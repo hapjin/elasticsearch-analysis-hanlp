7a378c3310972bed0b08bf28134909454a9047d9
hapjin <happypsjin@163.com>
2018-09-28 17:58:53 +0800

    add normalization for hanlp_standard analyzer

--- src/main/java/com/hankcs/lucene/HanLPStandardAnalyzer.java
+++ src/main/java/com/hankcs/lucene/HanLPStandardAnalyzer.java
@@ -26,6 +26,7 @@ public class HanLPStandardAnalyzer extends Analyzer {
 
     @Override
     protected Analyzer.TokenStreamComponents createComponents(String fieldName) {
+        HanLP.Config.Normalization = true;
         Tokenizer tokenizer = new HanLPTokenizer(HanLP.newSegment(), configuration);
         return new Analyzer.TokenStreamComponents(tokenizer);
     }
